819c1f4c3eef1abcaece781fe6ad3c7177158ed0
PriyankaJonnnala <144570902+PriyankaJonnnala@users.noreply.github.com>
2024-09-25 14:41:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
 from flask import Flask, render_template, redirect, url_for
 from models import db, Pet
 from forms import PetForm
@@ -9,13 +15,13 @@ db.init_app(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-  form = PetForm()
-  if form.validate_on_submit():
-     pet = Pet(name=form.name.data, age=form.age.data, type=form.type.data)
-     db.session.add(pet)
-     db.session.commit()
-     return redirect(url_for('index'))
-  pets = Pet.query.all()
-  return render_template('view_pets.html', form=form, pets=pets)
+    form = PetForm()
+    if form.validate_on_submit():
+	    pet = Pet(name=form.name.data, age=form.age.data, type=form.type.data)
+	    db.session.add(pet)
+	    db.session.commit()
+	    return redirect(url_for('index'))
+    pets = Pet.query.all()
+    return render_template('view_pets.html', form=form, pets=pets)
 if __name__ == '__main__':
-  app.run(debug=True)
+    app.run(debug=True)
